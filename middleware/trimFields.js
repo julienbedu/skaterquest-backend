cbe0e7bdb7bc28e8ca5b49c618dba8aa72b60d4c
HenryLF <henrylf@protonmail.com>
2025-04-14 09:37:44 +0100

    --created user models and populate database with dummydata --write middleware for token verif and fiel sanitazition

--- middleware/trimFields.js
+++ middleware/trimFields.js
@@ -1,10 +1,10 @@
 function trimBodyFields(...fields) {
-    return function (req, _, next) {
-      for (let field of fields) {
-        req.body[field] && (req.body[field] = req.body[field].trim()) 
-      }
-      next();
-    };
-  }
-  
-  module.exports = trimBodyFields;
+  return function (req, _, next) {
+    for (let field of fields) {
+      req.body[field] && (req.body[field] = req.body[field].trim());
+    }
+    next();
+  };
+}
+
+module.exports = trimBodyFields;
