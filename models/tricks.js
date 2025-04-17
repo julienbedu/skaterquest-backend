6f3e49b09ef90ec17dc0a44f2ecdd9024dfc1682
HenryLF <henrylf@protonmail.com>
2025-04-17 08:49:25 +0100

    change tricks model to thomas format

--- models/tricks.js
+++ models/tricks.js
@@ -1,24 +1,12 @@
 const mongoose = require("mongoose");
 
-const TRICKS_CATEGORY = [
-  "NOLLIE",
-  "FACKIE",
-  "SWITCH",
-  "SHUVIT",
-  "360-SHUVIT",
-  "180",
-  "360",
-  "OLD SCHOOL",
-  "GAP",
-  "KICKFLIP",
-  "HEELFLIP",
-];
+const TRICKS_CATEGORY = ["Flat", "Stairs", "Wall", "Grind"];
 
 const trickScheme = mongoose.Schema({
   name: String,
   description: String,
   level: { type: Number, min: 1, max: 3 },
-  category: [TRICKS_CATEGORY],
+  category: TRICKS_CATEGORY,
 });
 
 const Trick = mongoose.model("tricks", trickScheme);
