541a3421c511a1a9a6fc8f6685cc84b9341e7fcd
Your name <Your email>
2025-05-25 15:55:50 +0200

    Modifs dans api/index.js pour test

--- api/index.js
+++ api/index.js
@@ -1,2 +1,6 @@
-const app = require('./app');
-module.exports = app;
+// const app = require('./app');
+// module.exports = app;
+
+module.exports = (req, res) => {
+  res.status(200).json({ success: true, message: "Backend Vercel minimal OK" });
+};
