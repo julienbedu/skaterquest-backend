33e1ce8fad4c7bc3116ba5a0acc748af05ad72de
Your name <Your email>
2025-05-25 16:06:33 +0200

    Débugage Vercel en cours

--- api/index.js
+++ api/index.js
@@ -1,15 +1,4 @@
-// const app = require('./app');
-// module.exports = app;
-
-// module.exports = (req, res) => {
-//   res.status(200).json({ message: 'Fonction serverless basique OK' });
-// };
-
 const app = require('./app');
-const http = require('http');
+const serverless = require('serverless-http');
 
-const server = http.createServer(app);
-
-server.listen(process.env.PORT || 3000, () => {
-  console.log('Server listening on port', process.env.PORT || 3000);
-});
+module.exports = serverless(app);
